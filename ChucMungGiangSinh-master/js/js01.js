4cb5ac93604839cdc5b43e6e94c542310020785e
nnnnnnnnnnnnn22222212 <sonnltph21215@fpt.edu.vn>
2022-12-27 00:38:39 +0700

    uppload

---
+++ ChucMungGiangSinh-master/js/js01.js
@@ -0,0 +1,14 @@
+var james = $("#bond");
+
+var right = function() {
+  james.animate({ left: "5px" }, 600, left);
+};
+
+var left = function() {
+  james.animate({ left: "0px" }, 600, right);
+};
+right();
+ 
+$("#flip").click(function(){
+    $("#bond").toggle("slow");
+});
